0d91ab66d31f8075f562c5060dc91a7d3078bb1d
PeterBernath <peter.bernath.27@gmail.com>
2018-02-19 11:25:15 +0100

    Fixed logging

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +13,15 @@ public class Player {
     private static final Logger logger = LoggerFactory.getLogger(Player.class);
 
     public static int betRequest(JsonElement request) {
-        System.err.printf("%s", request.toString());
+        JsonObject ob = request.getAsJsonObject();
+        JsonArray arr = ob.getAsJsonArray("hole_cards");
+//        for (JsonElement jsonelement : arr) {
+//            String rank = jsonelement.getAsString();
+//            if (rank.indexOf("A") > 0) {
+//                return 1000;
+//            }
+//        }
+        logger.error("{}", request.toString());
         logger.error("{}", request.toString());
         return 1000;
     }
