7a230cb9c6691344ccbb83dfffc5a9dab12afed6
PeterBernath <peter.bernath.27@gmail.com>
2018-02-19 16:47:50 +0100

    Fixed logging

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 
 public class Player {
 
-    static final String VERSION = "0.14";
+    static final String VERSION = "0.15";
     private static final Logger logger = LoggerFactory.getLogger(Player.class);
 
     public static int betRequest(JsonElement request) {
@@ -28,9 +28,9 @@ public class Player {
             String color2 = secondCard.get("suit").toString();
             JSONArray community = json.getJSONArray("community_cards");
             if (rank1.matches("[10JQKA]") && rank1.equals(rank2)) return 100;
-            if (rank1.matches("[JQKA]") && rank2.matches("[JQKA]") && color1.equals(color2)) return 70;
-            if (rank1.matches("[910JQKA]") && rank2.matches("[910JQKA]")) return 35;
-            if (rank1.equals(rank2)) return 70;
+            if (rank1.matches("[JQKA]") && rank2.matches("[JQKA]") && color1.equals(color2)) return 50;
+            if (rank1.matches("[910JQKA]") && rank2.matches("[910JQKA]")) return 25;
+            if (rank1.equals(rank2)) return 50;
         } catch (Exception e) {
             e.printStackTrace();
         }
