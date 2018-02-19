d1535b4f14689f1ca0f68bce98d0841b67984679
PeterBernath <peter.bernath.27@gmail.com>
2018-02-19 14:05:19 +0100

    Fixed logging

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 
 public class Player {
 
-    static final String VERSION = "TryCatch5";
+    static final String VERSION = "TryCatch6";
     private static final Logger logger = LoggerFactory.getLogger(Player.class);
 
     public static int betRequest(JsonElement request) {
@@ -27,6 +27,7 @@ public class Player {
             JSONObject secondCard = cards2.getJSONObject(1);
             String rank1 = firstCard.get("rank").toString();
             String rank2 = secondCard.get("rank").toString();
+            if (rank1.equals("A") || rank2.equals("A")) return 500;
             logger.error("{}, {}", rank1, rank2);
         } catch (Exception e) {
             e.printStackTrace();
