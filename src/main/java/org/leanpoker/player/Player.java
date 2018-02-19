4456721fa42ddbfd4ff62a84e9264b487cb5f05b
PeterBernath <peter.bernath.27@gmail.com>
2018-02-19 14:19:38 +0100

    Fixed logging

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,14 +10,11 @@ import org.slf4j.LoggerFactory;
 
 public class Player {
 
-    static final String VERSION = "TryCatch7";
+    static final String VERSION = "TryCatch8";
     private static final Logger logger = LoggerFactory.getLogger(Player.class);
 
     public static int betRequest(JsonElement request) {
         try {
-
-//            String result = "{\"tournament_id\":\"5a78496392506b0004000003\",\"game_id\":\"5a8aafd0f2411c00040000cc\",\"round\":0,\"players\":[{\"name\":\"Spookers\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"time_used\":0,\"version\":\"8\",\"id\":0},{\"name\":\"PyChart\",\"stack\":998,\"status\":\"active\",\"bet\":2,\"time_used\":0,\"version\":\"Error check\",\"id\":1},{\"name\":\"Mazsolaaak\",\"stack\":996,\"status\":\"active\",\"bet\":4,\"time_used\":0,\"version\":\"NEM VAGYOK ROBOT 0.8\",\"id\":2},{\"name\":\"7Poker\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"hole_cards\":[{\"rank\":\"8\",\"suit\":\"clubs\"},{\"rank\":\"J\",\"suit\":\"diamonds\"}],\"time_used\":0,\"version\":\"Default Java folding player\",\"id\":3},{\"name\":\"ScriptEm\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"time_used\":0,\"version\":\"0.1\",\"id\":4},{\"name\":\"return 0\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"time_used\":0,\"version\":\"0.1\",\"id\":5}],\"small_blind\":2,\"big_blind\":4,\"orbits\":0,\"dealer\":0,\"community_cards\":[],\"current_buy_in\":4,\"pot\":6,\"in_action\":3,\"minimum_raise\":2,\"bet_index\":2}";
-//            JSONObject json = new JSONObject(result);
             JsonObject gson = request.getAsJsonObject();
             JSONObject json = new JSONObject(gson.toString());
             JSONArray players = json.getJSONArray("players");
@@ -29,6 +26,7 @@ public class Player {
             String rank2 = secondCard.get("rank").toString();
             if (rank1.equals("A") || rank2.equals("A") || rank1.equals(rank2)) return 500;
             logger.error("{}, {}", rank1, rank2);
+            logger.error("{}", json.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
